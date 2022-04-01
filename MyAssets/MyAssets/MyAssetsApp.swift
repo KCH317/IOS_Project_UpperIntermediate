ac1b9dab4e4ac5b4232128b145547c4e78ac2732
KCH317 <rnjscksgh94@gmail.com>
2022-04-01 20:40:53 +0900

    1. 자산관리앱
    Swift UI적용

---
+++ MyAssets/MyAssets/MyAssetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAssetsApp.swift
+//  MyAssets
+//
+//  Created by 권찬호 on 2022/04/01.
+//
+
+import SwiftUI
+
+@main
+struct MyAssetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
